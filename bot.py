edced32a2018f4d1822ba2e5e88b7d839b715be8
kalebavincent <kalebavincent@gmail.com>
2025-01-07 08:02:42 +0000

    bot engen

--- bot.py
+++ bot.py
@@ -1,11 +1,13 @@
 from flask import Flask, jsonify
 import threading
 import asyncio
+import os
 from platform import python_version
 from time import gmtime, strftime, time
-from pyrogram import Client, __version__, enums
+from pyrogram import Client, __version__, idle
 from pyrogram.raw.all import layer
 from bot.database import session, base
+import logging
 from bot import (
     STRING_SESSION,
     API_HASH,
@@ -18,81 +20,58 @@ from bot import (
     UPTIME,
     BOT_TOKEN)
 
+logging.basicConfig(
+    level=logging.WARNING, 
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.FileHandler("bot.log"),  # Écrit les logs dans bot.log
+        # logging.StreamHandler()  # Désactivé pour ne pas écrire dans la console
+    ]
+)
+
+logger = logging.getLogger(__name__)
+
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+logging.getLogger("werkzeug").setLevel(logging.ERROR)
+
 app = Flask(__name__)
 
-# Variable globale pour savoir si le bot est en ligne
-bot_online = False
+@app.route('/')
+def home():
+    return "Bot is running. Made with ♥️ by Hyoshdesign"
 
-class Bot(Client):
-    def __init__(self):
-        name = self.__class__.__name__.lower()
-        super().__init__(
-            STRING_SESSION,
-            plugins=dict(root=f"{name}.plugins"),
-            api_id=APP_ID,
-            api_hash=API_HASH,
-            bot_token=BOT_TOKEN,
-            workers=WORKERS,
-            parse_mode=enums.ParseMode.HTML
-        )
+def start_server():
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host="0.0.0.0", port=port)
+
+bot = Client(
+    "RenamerBot",
+    bot_token=BOT_TOKEN,
+    api_id=APP_ID,
+    api_hash=API_HASH,
+    plugins=dict(root='plugins')
+)
+
+
+
+def main():
+    if STRING_SESSION:
+        apps = [ bot]
         
-    async def start(self):
-        global bot_online
-        await super().start() 
-        LOGGER.info("Starting bot...")
-
-        me = await self.get_me()  
-        LOGGER.info(
-            f"Pyrogram v{__version__} (Layer - {layer}) started on {me.username} [{me.id}]",
-        )
-        LOGGER.info(f"Python Version: {python_version()}\n")
-        LOGGER.info("Bot Started Successfully!\n")
-        await asyncio.sleep(60)
-        await self.send_message(LOG_CHANNEL, "<i>Démarrage du bot...</i>")
+        for app_instance in apps:
+            app_instance.start()
+            logger.warning("Bot started. WARNING and ERROR logs are captured.")
         
-        bot_online = True  # Marquer le bot comme étant en ligne
-
-    async def stop(self):
-        global bot_online
-        runtime = strftime("%Hh %Mm %Ss", gmtime(time() - UPTIME))
-        LOGGER.info("Uploading logs before stopping...!\n")
-        await self.send_message(
-            LOG_CHANNEL,
-            ("Bot arrêté!\n\n"
-             f"Temps d'exécution: {runtime}\n"
-             f"<code>{LOG_DATETIME}</code>")
-        )
-
-        await super().stop()
-        LOGGER.info(
-            f"""Bot arrêté [Temps d'exécution: {runtime}s]\n"""
-        )   
-        bot_online = False  # Marquer le bot comme arrêté
-
-# Route Flask pour vérifier si le bot est en ligne
-@app.route("/check_live", methods=["GET"])
-def check_live():
-    if bot_online:
-        return jsonify({"status": "Bot is online"}), 200
+        idle()
+        
+        for app_instance in apps:
+            app_instance.stop()
     else:
-        return jsonify({"status": "Bot is offline"}), 503
-
-# Fonction pour démarrer Flask dans un thread séparé
-def run_flask():
-    app.run(host="0.0.0.0", port=5000, debug=False)
-
-# Démarrer le bot et le serveur Flask dans des threads séparés
-def start_bot_and_flask():
-    bot = Bot()
-
-    # Lancer Flask dans un thread séparé
-    flask_thread = threading.Thread(target=run_flask)
-    flask_thread.daemon = True
-    flask_thread.start()
-
-    # Démarrer le bot
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(bot.start())
+        bot.run()
 
 if __name__ == "__main__":
-    start_bot_and_flask()
+    from threading import Thread
+    server_thread = Thread(target=start_server)
+    server_thread.start()
+    
+    main()
