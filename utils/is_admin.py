d9c2d268ee4e2859d6d1ddcb9b7c0212ee0a1df2
kalebavincent <kalebavincent@gmail.com>
2025-01-07 08:55:07 +0000

    fixe bug add+channel

--- utils/is_admin.py
+++ utils/is_admin.py
@@ -1,10 +1,11 @@
-from pyrogram.enums import ChatMembersFilter  
+from pyrogram.enums import ChatMembersFilter
+
 async def is_bot_admin(bot, chat_id):
-    admins = []
+    # Obtenir les informations du bot
     me = await bot.get_me()
-
+    
+    # Vérifier parmi les administrateurs
     async for member in bot.get_chat_members(chat_id, filter=ChatMembersFilter.ADMINISTRATORS):
-        admins.append(member.user.username)
-
-    print(admins) 
-    return me.username in admins
+        if member.user.id == me.id:  # Comparer par ID, plus fiable
+            return True
+    return False
