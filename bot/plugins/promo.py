de31509e12fc6cbce3e9b5a9a669d5ad4eeff9ce
kalebavincent <kalebavincent@gmail.com>
2025-01-08 11:45:07 +0000

    boutto promo

--- bot/plugins/promo.py
+++ bot/plugins/promo.py
@@ -45,44 +45,7 @@ async def delete_promo_handler(bot:Client,message:Message):
     
 
 
-# @Bot.on_callback_query(filters.regex('^send_with_dynamic_columns$') & filters.user(get_admin()))
-# async def send_with_dynamic_columns(bot: Client, message: Message):
-#     try:
-#         buttons = get_buttons()
-#         promo = get_post()
 
-#         # Dynamically arrange buttons in columns (e.g., 2 buttons per row)
-#         columns = get_row()
-#         button_rows = []
-#         for idx, btn in enumerate(buttons):
-#             if hasattr(btn, 'name') and hasattr(btn, 'url'):
-#                 if idx % columns == 0:
-#                     button_rows.append([])
-#                 button_rows[-1].append(InlineKeyboardButton(btn.name, url=btn.url))
-
-#         # Add buttons to the InlineKeyboardMarkup
-#         reply_markup = InlineKeyboardMarkup(button_rows)
-        
-#         # Sending the promo message with dynamic buttons
-#         dest = f"{promo.set_top}\n\n{p}\n\n{promo.set_bottom}"
-#         await bot.send_message(
-#             chat_id=SUPPORT_CHANNEL,
-#             text=dest,
-#             reply_markup=reply_markup,
-#             disable_web_page_preview=True,
-#             parse_mode=enums.ParseMode.HTML
-#         )
-#         await message.reply("✅ Message envoyé avec des colonnes dynamiques.")
-#     except Exception as e:
-#         LOGGER.error(e)
-#         await bot.send_message(
-#             LOG_CHANNEL,
-#             f"<code>{traceback.format_exc()}</code>\n\nTemps : {time.ctime()} UTC",
-#             parse_mode=enums.ParseMode.HTML,
-#         )
-#         await message.reply("**⚠️ Une erreur est survenue lors de l'envoi du message.**", parse_mode=enums.ParseMode.MARKDOWN)
-
-# Variable globale pour stocker les IDs des messages envoyés
 sent_message_ids = []
 
 @Bot.on_callback_query(filters.regex('^send_with_dynamic_columns$') & filters.user(get_admin()))
@@ -324,50 +287,110 @@ async def send_desc_promo_handler(bot:Client,message:Message):
             await bot.send_message(message.message.chat.id,"**⚠️ Un problème est survenu**",parse_mode=enums.ParseMode.MARKDOWN)
             
             
-@Bot.on_callback_query(filters.regex('^send_button_promo$')& filters.user(get_admin()))
-async def send_button_promo_handler(bot:Client,message:Message):
+@Bot.on_callback_query(filters.regex('^send_button_promo$') & filters.user(get_admin()))
+async def send_button_promo_handler(bot: Client, message: Message):
     try:
-            channl=''
-            error_list=''
-            li=1
-            buttons=[]
-            down_buttons=get_buttons()
-            bq=[InlineKeyboardButton(x.name,url=x.url) for x in down_buttons]
-            buttons=[]
-            for i in a:
-                
-                userdata=""
-                
-                for j in i:
-                    ch=get_channel_by_id(j)
-                    user=get_user_username(ch.chat_id)
-                    channel_count=get_user_channel_count(ch.chat_id)
-                    buttons.append(InlineKeyboardButton(ch.description,url=ch.invite_link))
-                    
-                    userdata+=f'<code> @{user} ({channel_count})</code>\n'
-                markup=InlineKeyboardMarkup([buttons,bq])
-                forward= await bot.send_photo(SUPPORT_CHANNEL,'bot/downloads/image.jpg',caption=b.set_caption,
-                                    reply_markup=markup) 
-                await bot.send_message(SUPPORT_CHANNEL,f"Admin Liste {li}\n\n{userdata}")
-                li=li+1
-                for x in i:
-                    chname=get_channel_by_id(x)
-                    try:
-                        id_channel=await bot.forward_messages(chat_id=x,from_chat_id=SUPPORT_CHANNEL,message_ids=forward.message_id)
-                        save_message_ids(x,id_channel.message_id)
-                        channl+=f"✅ Nom du canal : {chname.channel_name}\nhttp://t.me/c/{str(x)[3:]}/{str(id_channel.message_id)}\n{line}\n\n"
-                        
-                    except (ChatAdminRequired,ChannelPrivate,ChatWriteForbidden,ChatForbidden):
-                        await bot.send_message(x.chat_id,f"Échec de l'envoi du message pour {x.channel_name}\nVeuillez republier la promotion pour éviter un bannissement")
-                        error_list+=f"🆔 ID : {x.channel_id}\n📛 Nom : {x.channel_name}\n👨‍ Admin : @{x.admin_username} \n🔗Lien : {x.invite_link}\n➖➖➖➖➖➖➖➖➖➖➖➖➖➖"
-                    except Exception as e:
-                        await bot.send_message(LOG_CHANNEL,f'<code>{traceback.format_exc()}</code>\n\nTemps : {time.ctime()} UTC',parse_mode=enums.ParseMode.HTML)
-                        LOGGER.error(e)
-                        
-                await bot.send_message(SUPPORT_GROUP,f"#shared sucessfull\n\n{channl} ",parse_mode=enums.ParseMode.MARKDOWN)
-                await bot.send_message(SUPPORT_GROUP,f"#unsucessfull\n\n{error_list}") 
-                
+        channl = ''
+        error_list = ''
+        li = 1
+        down_buttons = get_buttons()  
+        bq = [InlineKeyboardButton(x.name, url=x.url) for x in down_buttons]
+
+        for i in a:
+            userdata = ""
+            all_buttons = []
+            grouped_buttons = []
+            
+            for j in i:
+                ch = get_channel_by_id(j)
+                user = get_user_username(ch.chat_id)
+                channel_count = get_user_channel_count(ch.chat_id)
+
+                all_buttons.append(InlineKeyboardButton(ch.description, url=ch.invite_link))
+                userdata += f'<code>@{user} ({channel_count})</code>\n'
+
+            while all_buttons:
+                grouped_buttons += [all_buttons[:3]]  
+                all_buttons = all_buttons[3:]  
+
+                if len(grouped_buttons) == 3:
+                    grouped_buttons.append([InlineKeyboardButton("Ajouter ton canal", url=f"https://t.me/{bot.username}")])
+                    grouped_buttons += [all_buttons[:3]]  
+                    all_buttons = all_buttons[3:]
+            
+            grouped_buttons.append(bq)
+
+            markup = InlineKeyboardMarkup(grouped_buttons)
+
+            forward = await bot.send_photo(
+                SUPPORT_CHANNEL,
+                'bot/downloads/image.jpg',
+                caption=b.set_caption,
+                reply_markup=markup
+            )
+
+            await bot.send_message(SUPPORT_CHANNEL, f"Admin Liste {li}\n\n{userdata}")
+            li += 1
+            for x in i:
+                chname = get_channel_by_id(x)
+                try:
+                    id_channel = await bot.forward_messages(
+                        chat_id=x,
+                        from_chat_id=SUPPORT_CHANNEL,
+                        message_ids=forward.id
+                    )
+
+                    save_message_ids(x, id_channel.id)
+
+                    channl += (
+                        f"✅ Nom du canal : {chname.channel_name}\n"
+                        f"http://t.me/c/{str(x)[3:]}/{str(id_channel.id)}\n"
+                        f"{line}\n\n"
+                    )
+
+                except (ChatAdminRequired, ChannelPrivate, ChatWriteForbidden, ChatForbidden) as e:
+                    await bot.send_message(
+                        x.chat_id,
+                        f"Échec de l'envoi du message pour {chname.channel_name}\n"
+                        "Veuillez republier la promotion pour éviter un bannissement."
+                    )
+                    error_list += (
+                        f"🆔 ID : {x.channel_id}\n"
+                        f"📛 Nom : {chname.channel_name}\n"
+                        f"👨‍ Admin : @{chname.admin_username}\n"
+                        f"🔗 Lien : {chname.invite_link}\n"
+                        f"➖➖➖➖➖➖➖➖➖➖➖➖➖➖\n"
+                    )
+
+                except Exception as e:
+                    await bot.send_message(
+                        LOG_CHANNEL,
+                        f'<code>{traceback.format_exc()}</code>\n\nTemps : {time.ctime()} UTC',
+                        parse_mode=enums.ParseMode.HTML
+                    )
+                    LOGGER.error(e)
+
+            await bot.send_message(
+                SUPPORT_GROUP,
+                f"#shared_successful\n\n{channl}",
+                parse_mode=enums.ParseMode.MARKDOWN
+            )
+            if error_list:
+                await bot.send_message(
+                    SUPPORT_GROUP,
+                    f"#unsuccessful\n\n{error_list}",
+                    parse_mode=enums.ParseMode.MARKDOWN
+                )
+
     except Exception as e:
-            LOGGER.error(e)
-            await bot.send_message(LOG_CHANNEL,f'\n<code>{traceback.format_exc()}</code>\n\nTemps : {time.ctime()} UTC',parse_mode=enums.ParseMode.HTML)
-            await bot.send_message(message.message.chat.id,"**⚠️ Un problème est survenu**",parse_mode=enums.ParseMode.MARKDOWN)
+        LOGGER.error(e)
+        await bot.send_message(
+            LOG_CHANNEL,
+            f'<code>{traceback.format_exc()}</code>\n\nTemps : {time.ctime()} UTC',
+            parse_mode=enums.ParseMode.HTML
+        )
+        await bot.send_message(
+            message.chat.id,
+            "**⚠️ Un problème est survenu**",
+            parse_mode=enums.ParseMode.MARKDOWN
+        )
